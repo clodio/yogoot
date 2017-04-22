ddf1b29bf144a730e7b600a969426183338b0e34
Claude Seguret <claude.seguret@laposte.fr>
2017-04-22 08:55:32 +0200

    add test

---
+++ routes/test.js
@@ -0,0 +1,121 @@
+[{
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [7.769801, 48.569154]
+	},
+	"properties": {
+		"_id": "33922676501",
+		"id_contract_individual": "33922676502",
+		"id_contract_group": "339226765",
+		"contract_order_nb": 2
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [4.856372, 45.811601]
+	},
+	"properties": {
+		"_id": "33922678101",
+		"id_contract_individual": "33922680101",
+		"id_contract_group": "339226801",
+		"contract_order_nb": 1
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [3.548204, 47.798343]
+	},
+	"properties": {
+		"_id": "33922681301",
+		"id_contract_individual": "33922683501",
+		"id_contract_group": "339226835",
+		"contract_order_nb": 1
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [-1.017768, 46.19249]
+	},
+	"properties": {
+		"_id": "33922687401",
+		"id_contract_individual": "33922688001",
+		"id_contract_group": "339226880",
+		"contract_order_nb": 1
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [1.872344, 49.047045]
+	},
+	"properties": {
+		"_id": "33922689501",
+		"id_contract_individual": "33922690001",
+		"id_contract_group": "339226900",
+		"contract_order_nb": 1
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [1.872344, 49.047045]
+	},
+	"properties": {
+		"_id": "33922690001",
+		"id_contract_individual": "33922690002",
+		"id_contract_group": "339226900",
+		"contract_order_nb": 2
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [0.482015, 49.578212]
+	},
+	"properties": {
+		"_id": "33922690501",
+		"id_contract_individual": "33922690502",
+		"id_contract_group": "339226905",
+		"contract_order_nb": 2
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [5.808627, 43.127013]
+	},
+	"properties": {
+		"_id": "33922690502",
+		"id_contract_individual": "33922690601",
+		"id_contract_group": "339226906",
+		"contract_order_nb": 1
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [-1.833056, 47.036968]
+	},
+	"properties": {
+		"_id": "33922696301",
+		"id_contract_individual": "33922696401",
+		"id_contract_group": "339226964",
+		"contract_order_nb": 1
+	}
+}, {
+	"type": "Feature",
+	"geometry": {
+		"type": "Point",
+		"coordinates": [4.968606, 45.559783]
+	},
+	"properties": {
+		"_id": "33922701102",
+		"id_contract_individual": "33922703001",
+		"id_contract_group": "339227030",
+		"contract_order_nb": 1
+	}
+}]
